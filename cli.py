16f704a0dbcc5ab4d8a0204ba6548b8e89543bbe
ave2me <ave2@me.com>
2019-10-09 02:04:56 +0300

    add verifications & improve help

--- cli.py
+++ cli.py
@@ -2,39 +2,118 @@ from json import dump, load
 
 import click
 
-from giveaway import filter_usernames, prepare_username, process_usernames, choose_winner, find_winner
+from .giveaway import (
+    filter_usernames,
+    prepare_username,
+    process_usernames,
+    choose_winner,
+    find_winner,
+    verify_winner,
+    hash_username,
+    UserName)
 
 
 @click.group()
 def cli():
+    """
+    Program to choose a winner from a given list of participants. The seed used by PRNG depends only on a list of
+    participants and a date provided. So for every set of parameters the giveaway results can be reproduced.
+    """
     pass
 
 
-@cli.command('prepare')
-@click.argument('inputfile', type=click.Path(exists=True, file_okay=True, dir_okay=False))
-@click.argument('outputfile', type=click.Path(file_okay=True, dir_okay=False))
-def prepare_raw_cli(inputfile, outputfile):
-    with open(inputfile) as fp:
+@cli.command("prepare")
+@click.argument("source", type=click.Path(exists=True, file_okay=True, dir_okay=False))
+@click.argument("destination", type=click.Path(file_okay=True, dir_okay=False))
+def prepare_original_cli(source, destination):
+    """Parse a file with a of participants, clean it up and save to json"""
+    with open(source) as fp:
         lines = (line for line in fp)
         valid_lines = filter_usernames(lines)
         prepared_usernames = [prepare_username(uname) for uname in valid_lines]
 
-    with open(outputfile, 'w') as ofp:
+    with open(destination, "w") as ofp:
         dump(prepared_usernames, ofp)
 
 
-@cli.command('choose')
-@click.argument('participants', type=click.Path(exists=True, file_okay=True, dir_okay=False))
-@click.argument('date', type=click.DateTime(formats=['%d-%m-%Y']))
+@cli.command("choose")
+@click.argument(
+    "participants", type=click.Path(exists=True, file_okay=True, dir_okay=False)
+)
+@click.argument("date", type=click.DateTime(formats=["%d-%m-%Y"]))
 def choose_winner_cli(participants, date):
-    with open(participants, 'r') as fp:
+    """choose a winner from PARTICIPANTS while using DATE to count seed"""
+    with open(participants) as fp:
         raw_usernames = load(fp)
-        participants = [prepare_username(uname) for uname in raw_usernames]
+    hashed_participants = process_usernames(raw_usernames)
+    hashed_winner = choose_winner(hashed_participants, date)
+    participants = [prepare_username(uname) for uname in raw_usernames]
+    winner = find_winner(hashed_winner, participants)
+    click.echo(winner)
+
+
+@cli.command("prepare_hashed")
+@click.argument("source", type=click.Path(exists=True, file_okay=True, dir_okay=False))
+@click.argument("destination", type=click.Path(file_okay=True, dir_okay=False))
+def prepare_hashed_cli(source, destination):
+    """Hash a list of participants from SOURCE and save to DESTINATION"""
+    with open(source) as fp:
+        raw_usernames = load(fp)
         hashed_participants = process_usernames(raw_usernames)
-        hashed_winner = choose_winner(hashed_participants, date)
-        winner = find_winner(hashed_winner, participants)
-        click.echo(winner)
+
+    with open(destination, "w") as ofp:
+        dump(hashed_participants, ofp)
 
 
-if __name__ == '__main__':
+@cli.command("verify_choice")
+@click.argument(
+    "hashed_participants", type=click.Path(exists=True, file_okay=True, dir_okay=False)
+)
+@click.argument("date", type=click.DateTime(formats=["%d-%m-%Y"]))
+@click.option(
+    "--username", default=None, help="username to compare with a winner's hash"
+)
+def verify_choice_cli(hashed_participants, date, username):
+    """
+    Verify choice using a HASHED_PARTICIPANTS file and DATE. Optionally you can provide a username to verify
+    that it was chosen.
+    """
+    with open(hashed_participants) as fp:
+        hashed_participants = load(fp)
+    hashed_winner = choose_winner(hashed_participants, date)
+    click.echo(f"Winner's hash is {hashed_winner}.")
+    if username:
+        prepared_username = prepare_username(username)
+        is_winner = verify_winner(prepared_username, hashed_winner)
+        if is_winner:
+            click.echo(f"Yup! {username} is definitely a winner.")
+        else:
+            click.echo(
+                f"Unfortunately, {username} is not a winner :(. But don't worry, better luck next time!"
+            )
+
+
+@cli.command("verify_participant")
+@click.argument(
+    "hashed_participants", type=click.Path(exists=True, file_okay=True, dir_okay=False)
+)
+@click.argument("username", type=UserName)
+def verify_choice_cli(hashed_participants, username):
+    """
+    Verify given USERNAME is present in a HASHED_PARTICIPANTS file.
+    """
+    with open(hashed_participants) as fp:
+        hashed_participants = load(fp)
+    hashed_username = hash_username(prepare_username(username))
+    click.echo(f"Hashed username: {hashed_username}")
+    is_in_participants = hashed_username in hashed_participants
+    if is_in_participants:
+        click.echo(
+            f"{username} may not be a winner. But it is present in a list of participants."
+        )
+    else:
+        click.echo(f"{username} is a creep and does not belong here :(.")
+
+
+if __name__ == "__main__":
     cli()
--- giveaway.py
+++ giveaway.py
@@ -4,14 +4,14 @@ from hashlib import sha256
 from random import choice, seed
 from typing import List, Iterable, Optional, NewType, Generator, Any, Iterator
 
-ENCODING = 'utf-8'
-VALID_USERNAME = re.compile(r'^@?[0-9A-Za-z_]+$')
+ENCODING = "utf-8"
+VALID_USERNAME = re.compile(r"^@?[0-9A-Za-z_]+$")
 
-Line = NewType('Line', str)
-RawUserName = NewType('RawUserName', str)
-UserName = NewType('UserName', str)
-HashedUserName = NewType('HashedUserName', str)
-Seed = NewType('Seed', str)
+Line = NewType("Line", str)
+RawUserName = NewType("RawUserName", str)
+UserName = NewType("UserName", str)
+HashedUserName = NewType("HashedUserName", str)
+Seed = NewType("Seed", str)
 
 
 def count_seed(hashed_participants: List[HashedUserName], date: datetime) -> Seed:
@@ -25,12 +25,14 @@ def count_seed(hashed_participants: List[HashedUserName], date: datetime) -> See
     for participant in hashed_participants:
         h.update(participant.encode(ENCODING))
     ts = date.replace(tzinfo=timezone.utc).timestamp()
-    binary_ts = int(ts).to_bytes(32, 'big')
+    binary_ts = int(ts).to_bytes(32, "big")
     h.update(binary_ts)
     return h.hexdigest()
 
 
-def choose_winner(hashed_participants: List[HashedUserName], date: datetime) -> HashedUserName:
+def choose_winner(
+    hashed_participants: List[HashedUserName], date: datetime
+) -> HashedUserName:
     """
     Chooses a winner from a provided list of participants. Note, that to find out the real username we need to call a
     find_winner after all.
@@ -42,7 +44,7 @@ def choose_winner(hashed_participants: List[HashedUserName], date: datetime) -> 
 
 
 def prepare_username(uname: RawUserName) -> UserName:
-    _, sep, name = uname.lower().rpartition('@')
+    _, sep, name = uname.lower().rpartition("@")
     return UserName(name)
 
 
@@ -53,7 +55,9 @@ def hash_username(item: UserName) -> HashedUserName:
 
 def process_usernames(unames: List[RawUserName]) -> List[HashedUserName]:
     prepared: Iterable[UserName] = (prepare_username(uname) for uname in unames)
-    hashed: Iterable[HashedUserName] = sorted(hash_username(uname) for uname in prepared)
+    hashed: Iterable[HashedUserName] = sorted(
+        hash_username(uname) for uname in prepared
+    )
     result: List[HashedUserName] = list(hashed)
     return result
 
@@ -63,7 +67,9 @@ def verify_winner(uname: UserName, hashed_uname: HashedUserName) -> bool:
     return calculated == hashed_uname
 
 
-def find_winner(hashed_uname: HashedUserName, participants: List[UserName]) -> Optional[UserName]:
+def find_winner(
+    hashed_uname: HashedUserName, participants: List[UserName]
+) -> Optional[UserName]:
     """
     Given a hashed username and a list of participants' usernames returns a winners' username.
     """
